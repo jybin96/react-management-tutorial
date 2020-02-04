1a7378315da6b8c6fb0e16db7938249238fea02f
jybin96 <jybin96@naver.com>
2020-02-05 03:26:15 +0900

    add get customers rest api module

--- server.js
+++ server.js
@@ -6,9 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello',(req,res)=>{
-    res.send({message: 'hellow Express!'});
-
-});
+app.get('/api/customers',(req,res)=>{
+    res.send([
+        {
+        'id':1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name' : '정영빈',
+        'birthday': '961002',
+        'gender': '남자',
+        'job': '대학생'
+      },
+      {
+        'id':2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name' : '김소영',
+        'birthday': '960913',
+        'gender': '여자',
+        'job': '대학생'
+      },
+      {
+        'id':3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name' : '정영빈',
+        'birthday': '961222',
+        'gender': '남자',
+        'job': '디자이너'
+      }
+      ]);
+})
 
 app.listen(port,()=> console.log(`listening on port ${port}`));
