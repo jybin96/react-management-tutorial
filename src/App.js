606d23861d184c3b4f85aa0753df850ea221aaff
jybin96 <jybin96@naver.com>
2020-02-04 19:43:17 +0900

    create customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers =[
+  {
+  'id':1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name' : '정영빈',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id':2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name' : '김소영',
+  'birthday': '960913',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id':3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name' : '정영빈',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '디자이너'
+}
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+             key={c.id}
+             id={c.id}
+             image={c.image}
+             name={c.name}
+             birthday={c.birthday}
+             gender={c.gender}
+             job={c.job}
+             />
+          )
+        })
+      }
+   
     </div>
   );
 }
